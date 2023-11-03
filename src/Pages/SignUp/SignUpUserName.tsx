64fa1c02f2ecccced7623bef232b2fc2bd0aca58
AhaanSehgal <149166091+AhaanSehgal@users.noreply.github.com>
2023-11-03 20:27:05 +0530

    Update SignUpUserName.tsx

--- src/Pages/SignUp/SignUpUserName.tsx
+++ src/Pages/SignUp/SignUpUserName.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import Nav from '../../Components/SignUp/Nav';
 import { useNavigate } from 'react-router-dom';
 import Footer from '../../Components/Footer';
 import { KeyringController } from '@tria-sdk/web';
 import axios from "axios"
 import { useParams } from 'react-router-dom'
+import NavContext from '../../NavContext';
 
 
 export default function SignUp() {
@@ -18,13 +19,16 @@ export default function SignUp() {
 
   const baseUrl = 'https://staging.tria.so';
 
+  const { token } = useContext(NavContext)
+
   const [recommendations, setRecommendations] = useState([])
   const [available, setAvailable] = useState()
   const [name, setName] = useState("")
-  
+  const [loader, setLoader] = useState(false)
+
   const userId = useParams()
 
-  console.log("id",userId)
+  console.log("id", userId)
   const walletType = {
     embedded: true,
   };
@@ -53,22 +57,34 @@ export default function SignUp() {
   const [Id, setId] = useState('');
 
   async function createAccountWithoutPassword() {
-    const keyringController = new KeyringController({
-      baseUrl,
-      walletType,
-    });
+    setLoader(true)
+    try {
+      const keyringController = new KeyringController({
+        baseUrl,
+        walletType,
+      });
 
-    const res = await keyringController.socialogin({
-      triaName: name,
-      platform: 'google',
-       //@ts-ignore
-      userId: userId?.param,
-      isPasswordLess: true,
-      //@ts-ignore
-      password: null,
-    });
-    console.log('res', res);
-    // setUserData({ address: res.address });
+      const res = await keyringController.socialogin({
+        triaName: name,
+        platform: 'google',
+        //@ts-ignore
+        userId: userId?.param,
+        isPasswordLess: true,
+        //@ts-ignore
+        password: null,
+        accessToken: token,
+      });
+      console.log('res', res.success);
+      if (res.success === true) {
+        window.close()
+      } else {
+        setLoader(false)
+      }
+      // setUserData({ address: res.address });
+    } catch (err) {
+      setLoader(false)
+      console.log(err)
+    }
   }
   const navigate = useNavigate();
 
@@ -120,7 +136,14 @@ export default function SignUp() {
                   </div>
                   <div className="w-[99px] h-10 px-5 py-3 mix-blend-difference bg-white bg-opacity-90 rounded-[20px] justify-center items-center flex">
                     <div className="justify-center items-center flex">
-                      <button onClick={() => { createAccountWithoutPassword(); }}> <div className="text-center text-stone-950 text-base font-semibold font-Montserrat leading-tight">Next</div></button>
+                      <button onClick={() => { createAccountWithoutPassword(); }}> <div className="text-center text-stone-950 text-base font-semibold font-Montserrat leading-tight">{loader === false ? <span>Next</span> :
+                        <div className='ml-2' role="status">
+                          <svg aria-hidden="true" className="w-4 h-4 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                          </svg>
+                        </div>
+                      }</div></button>
                     </div>
                   </div>
                 </div>
@@ -146,7 +169,7 @@ export default function SignUp() {
                   <div className='flex gap-3 items-center w-[376px] overflow-x-scroll h-20 '>
                     {recommendations?.map((item, index) => {
                       return (
-                        <div onClick={() => {setName(item); getNameRecommendations(item); checkIfAvailable(item)}} key={index} className="self-stretch cursor-pointer justify-start items-center gap-2 inline-flex mt-3">
+                        <div onClick={() => setName(item)} key={index} className="self-stretch cursor-pointer justify-start items-center gap-2 inline-flex mt-3">
                           <div className='bg-gray-100 rounded-full px-5 py-2 font-Montserrat'>
                             {item}
                           </div>
