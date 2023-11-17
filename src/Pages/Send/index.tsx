58c99139d91f6600279bb17a9cf4340da691bd24
Lalit-threely <147034093+Lalit-threely@users.noreply.github.com>
2023-11-17 08:39:53 +0530

    ui fixes in send flow

--- src/Pages/Send/index.tsx
+++ src/Pages/Send/index.tsx
@@ -114,6 +114,7 @@ export default function SendAsset(props: any) {
   const [totalAmountIncrypto, setTotalAmountIncrypto] = useState<number>();
   const [feeLoading, setFeeLoading] = useState<boolean>(false);
   const [approveLoading, setApproveLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
   console.log("gasfees---------->", gasFees);
   const param = useParams();
   console.log("pa", param);
@@ -136,10 +137,10 @@ export default function SendAsset(props: any) {
       await wallet.init();
       const txn = await wallet.send(payload, params?.chainName);
       console.log("txawait--------------->", txn);
-      const x=  await txn?.data?.wait();
-      console.log("x------------------->",x);
+      const x = await txn?.data?.wait();
+      console.log("x------------------->", x);
       const res = await wallet.waitForTransaction(txn);
-     
+
       console.log("txres-------------------------->", res);
     } catch (err) {
       console.log(err);
@@ -149,9 +150,16 @@ export default function SendAsset(props: any) {
   };
 
   const getTriaName = async (address: any, chainName: any) => {
+    try{
     const triaName = await getUserByAddress(address, chainName);
+    if(triaName){
     setRecieverTriaName(triaName);
+    }
     console.log("triaName", triaName);
+    }
+    catch(err){
+      console.log("error",err);
+    }
   };
 
   const getSendFee = async (feeCallData) => {
@@ -180,12 +188,14 @@ export default function SendAsset(props: any) {
         setTotalAmountIncrypto(
           parseFloat(res?.fee?.eth || "0") + (params?.enteredAmountValue || 0)
         );
+        setFeeLoading(false);
+      }
+      else{
+        setError(res?.message ||"");
       }
       console.log({ res });
     } catch (err) {
       console.error(err);
-    } finally {
-      setFeeLoading(false);
     }
   };
 
@@ -208,6 +218,7 @@ export default function SendAsset(props: any) {
       getTriaName(jsonData?.recepientAddress, jsonData?.chainName);
       getAsset(jsonData);
       setParams(jsonData);
+      setRecieverTriaName(jsonData.recepientAddress);
     }
   };
 
@@ -351,6 +362,11 @@ export default function SendAsset(props: any) {
                     {params?.enteredAmountValue} {tokenDetails?.symbol}
                   </div>
                 </div>
+                <div className="w-100 h-[18px] justify-center items-center gap-2 inline-flex">
+                  <div className="text-center text-red-500 text-sm font-medium font-['Montserrat'] ">
+                    {error}
+                  </div>
+                </div>
               </div>
               <div className="w-[376px] justify-center items-start gap-4 inline-flex">
                 <div className="grow shrink basis-0 py-3 flex-col justify-center items-center gap-3 inline-flex">
@@ -412,30 +428,52 @@ export default function SendAsset(props: any) {
                     </div>
                   </div>
                 </div>
-                <div className="grow shrink basis-0 flex-col justify-start items-start gap-2 inline-flex">
-                  <div className="self-stretch justify-end items-center gap-2 inline-flex">
-                    <div className="text-center text-stone-950 text-opacity-60 text-lg font-normal font-montserrat leading-snug dark:text-text">
-                      ${gasFees?.usd?.substring(0, 7)}
+                {feeLoading ? (
+                  <div className="text-center text-stone-950 text-base font-semibold font-Montserrat leading-tight">
+                    {" "}
+                    <svg
+                      aria-hidden="true"
+                      className="w-4 h-4 mt-4  text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentFill"
+                      />
+                    </svg>
+                  </div>
+                ) : (
+                  <div className="grow shrink basis-0 flex-col justify-start items-start gap-2 inline-flex">
+                    <div className="self-stretch justify-end items-center gap-2 inline-flex">
+                      <div className="text-center text-stone-950 text-opacity-60 text-lg font-normal font-montserrat leading-snug dark:text-text">
+                        ${gasFees?.usd?.substring(0, 7)}
+                      </div>
+                      <div className="w-[18px] h-[18px] relative">
+                        <div className="font-montserrat">
+                          <img
+                            className="dark:invisible visible dark:w-0"
+                            src="/icons/setting-2.svg"
+                          ></img>
+                          <img
+                            className="dark:visible  invisible w-0 dark:w-[18px]"
+                            src="/icons/setting-2-dark.svg"
+                          ></img>
+                        </div>
+                      </div>
                     </div>
-                    <div className="w-[18px] h-[18px] relative">
-                      <div className="font-montserrat">
-                        <img
-                          className="dark:invisible visible dark:w-0"
-                          src="/icons/setting-2.svg"
-                        ></img>
-                        <img
-                          className="dark:visible  invisible w-0 dark:w-[18px]"
-                          src="/icons/setting-2-dark.svg"
-                        ></img>
+                    <div className="self-stretch justify-end items-center gap-2 inline-flex">
+                      <div className="text-center text-stone-950 text-opacity-60 text-sm font-normal font-montserrat leading-[16.80px] dark:text-text">
+                        {gasFees?.eth.substring(0, 7)} {tokenDetails?.symbol}
                       </div>
                     </div>
                   </div>
-                  <div className="self-stretch justify-end items-center gap-2 inline-flex">
-                    <div className="text-center text-stone-950 text-opacity-60 text-sm font-normal font-montserrat leading-[16.80px] dark:text-text">
-                      {gasFees?.eth.substring(0, 7)} {tokenDetails?.symbol}
-                    </div>
-                  </div>
-                </div>
+                )}
               </div>
               <div className="self-stretch py-3 justify-start items-start gap-4 inline-flex">
                 <div className="grow shrink basis-0 h-[22px] justify-start items-center gap-2 flex">
@@ -443,14 +481,36 @@ export default function SendAsset(props: any) {
                     Total Cost
                   </div>
                 </div>
-                <div className="grow shrink basis-0 flex-col justify-center items-end gap-1 inline-flex">
-                  <div className="text-center text-stone-950 text-opacity-60 text-lg font-medium font-montserrat leading-snug dark:text-text">
-                    ${totalAmountInUSD?.toFixed(6)}
+                {feeLoading ? (
+                  <div className="text-center text-stone-950 text-base font-semibold font-Montserrat leading-tight">
+                    {" "}
+                    <svg
+                      aria-hidden="true"
+                      className="w-4 h-4  text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentFill"
+                      />
+                    </svg>
                   </div>
-                  <div className="text-center text-stone-950 text-opacity-60 text-sm font-normal font-montserrat leading-[16.80px] dark:text-text">
-                    {totalAmountIncrypto?.toFixed(8)} {tokenDetails?.symbol}
+                ) : (
+                  <div className="grow shrink basis-0 flex-col justify-center items-end gap-1 inline-flex">
+                    <div className="text-center text-stone-950 text-opacity-60 text-lg font-medium font-montserrat leading-snug dark:text-text">
+                      ${totalAmountInUSD?.toFixed(6)}
+                    </div>
+                    <div className="text-center text-stone-950 text-opacity-60 text-sm font-normal font-montserrat leading-[16.80px] dark:text-text">
+                      {totalAmountIncrypto?.toFixed(8)} {tokenDetails?.symbol}
+                    </div>
                   </div>
-                </div>
+                )}
               </div>
               <div className="w-[376px] px-2 justify-center items-center gap-2 inline-flex">
                 <div className="justify-center items-center gap-0.5 flex">
